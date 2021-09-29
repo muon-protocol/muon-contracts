9e717ab84f59d621ad42bcb19ee44e63b55b6a88
Zahra Namazi <namazi.q@gmail.com>
2021-09-29 13:47:57 +0330

    change deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,31 +1,31 @@
-var muon = artifacts.require('./MuonV01.sol')
+var muon = artifacts.require('./MuonV02.sol')
 
-function parseArgv(){
-	let args = process.argv.slice(2);
-	let params = args.filter(arg => arg.startsWith('--'))
-	let result = {}
-	params.map(p => {
-		let [key, value] = p.split('=');
-		result[key.slice(2)] = value === undefined ? true : value
-	})
-	return result;
+function parseArgv() {
+  let args = process.argv.slice(2)
+  let params = args.filter((arg) => arg.startsWith('--'))
+  let result = {}
+  params.map((p) => {
+    let [key, value] = p.split('=')
+    result[key.slice(2)] = value === undefined ? true : value
+  })
+  return result
 }
 
 module.exports = function (deployer) {
-	deployer.then(async () => {
-		// let params = parseArgv()
+  deployer.then(async () => {
+    // let params = parseArgv()
 
-		await deployer.deploy(muon);
+    await deployer.deploy(muon)
 
-		// let muonAddress = null
-		// if(!!params['muonAddress']){
-		// 	muonAddress = params['muonAddress'];
-		// }
-		// else{
-		// 	let deployedMuon = await await deployer.deploy(muon);
-		// 	muonAddress = deployedMuon.address;
-		// }
-		
+    // let muonAddress = null
+    // if(!!params['muonAddress']){
+    // 	muonAddress = params['muonAddress'];
+    // }
+    // else{
+    // 	let deployedMuon = await await deployer.deploy(muon);
+    // 	muonAddress = deployedMuon.address;
+    // }
+
     //let deployedPresale = await deployer.deploy(presale, "0xd0e5D73785A1b179628F873306Ae5911dD29Aed3")
-	})
+  })
 }
