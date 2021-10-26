3d99fb466b215c5eb2afa5ade1660e69b52898cd
Sadegh Teymouri <sadeghte@gmail.com>
2021-10-26 15:42:50 +0330

    multi group init

---
+++ test/test-group-manager.js
@@ -0,0 +1,15 @@
+const truffleAssert = require('truffle-assertions');
+const {expect, muonNode} = require('./helpers')
+const {toBN} = web3.utils;
+
+contract("GroupManager", (accounts) => {
+    let owner=accounts[9], muon;
+
+    before(async () => {
+    });
+
+    describe("Test Smaple", async () => {
+        it("should verify signature", async () => {
+        })
+    })
+});
