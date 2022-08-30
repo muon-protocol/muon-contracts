53a3b01c2b000766ea03b0422fbbd679400ca001
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2022-08-31 00:55:58 +0430

    v4 contracts initial version

---
+++ contracts/MuonV04.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./utils/SchnorrSECP256K1Verifier.sol";
+import "./interfaces/IMuonV04.sol";
+
+contract MuonV04 is IMuonV04,
+    Ownable, SchnorrSECP256K1Verifier {
+
+    // event Transaction(bytes reqId, address[] groups);
+
+    // struct PublicKey {
+    //     uint256 x;
+    //     uint8 parity;
+    // }
+
+    // struct SchnorrSign {
+    //     uint256 signature;
+    //     address owner;
+    //     address nonce;
+    // }
+
+    // mapping(address => PublicKey) public groupsPubKey;
+
+    constructor(){
+    }
+
+    function verify(
+        bytes calldata reqId,
+        uint256 hash, 
+        SchnorrSign calldata signature,
+        PublicKey calldata pubKey
+    ) public override returns (bool) {
+        // TODO: need to make sure that pubKey
+        // is a Muon TSS key.
+        // Now it verifies any valid SchnorrSign
+        if(!verifySignature(pubKey.x, pubKey.parity, 
+                signature.signature, 
+                hash, signature.nonce)){
+            return false;
+        }
+        emit Transaction(reqId, pubKey);
+        return true;
+    }
+}
